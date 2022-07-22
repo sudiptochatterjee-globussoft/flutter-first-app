1d41439f1d538a9b8044ce54747261f6b7a95983
sudipto <sudipto.chatterjee@globussoft.in>
2022-07-22 16:10:18 +0530

    inkwell, async await, basic animation.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,19 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_one/utils/routes.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeBtn = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +34,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Login",
+                "Login $name",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -41,6 +49,11 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: const InputDecoration(
                           hintText: "Enter username", labelText: "Username"),
+                      onChanged: (value) {
+                        //same as on text change listener
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -50,13 +63,48 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
+                    InkWell(
+                      //using inkwell for click and animation on container
+                      onTap: () async {
+                        setState(() {
+                          changeBtn = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      child: Text("Login"),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeBtn ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeBtn
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            //shape: changeBtn ? BoxShape.circle : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changeBtn ? 50 : 8)),
+                      ),
                     ),
+                    //for making animatedbtn commented this for now
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   child: Text("Login"),
+                    // ),
                     SizedBox(
                       height: 10.0,
                     ),
