525941c1a9cb3ef21dfe0dcf70d963a298f26f2a
sudipto <sudipto.chatterjee@globussoft.in>
2022-07-27 16:50:08 +0530

    day 12 commit, data class generator, list view

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,18 +1,19 @@
 class CatlogModel {
-  static final items = [
-    Item(
-        id: "item001",
-        name: "iphone 12 pro",
-        desc: "Apple iphone 12 generation",
-        price: 79999,
-        color: "w33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
+  static List<Item> items = [];
+  // = [
+  //   Item(
+  //       id: 0,
+  //       name: "iphone 12 pro",
+  //       desc: "Apple iphone 12 generation",
+  //       price: 999,
+  //       color: "w33505a",
+  //       image:
+  //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  // ];
 }
 
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -26,4 +27,24 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_one/models/catalog.dart';
 import 'package:flutter_application_one/widgets/item_widget.dart';
 
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "code ninja";
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+  final String name = "code ninja";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catlog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatlogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(10, ((index) => CatlogModel.items[0]));
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -20,16 +44,19 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatlogModel.items != null && CatlogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatlogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatlogModel.items[index],
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
     );
   }
 }
+//4:33
