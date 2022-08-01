d863ef677b2f4b752e1d3af8704f17e200a6c91d
sudipto <sudipto.chatterjee@globussoft.in>
2022-08-01 13:45:10 +0530

    day-19 some fixes

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -26,7 +26,10 @@ class MyTheme {
           color: Colors.black,
           elevation: 0.0,
           iconTheme: IconThemeData(color: Colors.white),
-          titleTextStyle: Theme.of(context).textTheme.headline6));
+          titleTextStyle: Theme.of(context)
+              .textTheme
+              .headline6
+              ?.copyWith(color: Colors.white)));
 
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkCreamColor = Vx.gray900;
